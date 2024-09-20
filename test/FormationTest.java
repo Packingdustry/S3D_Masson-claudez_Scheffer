f029b44de1bb873c59135d10a19404bc931ff646
Benj <benjamin.scheffer7@etu.univ-lorraine.fr>
2024-09-20 16:25:16 +0200

    Ecriture des tests

--- test/FormationTest.java
+++ test/FormationTest.java
@@ -22,18 +22,36 @@ class FormationTest {
 
     @Test
     void modifier() {
+        //Modif simple
         f.ajouter("Math",1);
         f.modifier("Math",2);
         assertEquals(f.getMatEnseign().get("Math"),2.00);
+        //modifier quelque chose qui n'existe pas
         f.modifier("English",3);
         assertEquals(f.getMatEnseign().get("English"),null);
     }
 
     @Test
     void supprimer() {
+        f.ajouter("Math",1);
+        f.ajouter("English",2);
+        //suppression simple
+        f.supprimer("Math");
+        assertEquals(f.getMatEnseign().get("Math"),null);
+        //suppression simple
+        f.supprimer("English");
+        assertEquals(f.getMatEnseign().get("English"),null);
+
     }
 
     @Test
     void accesCoeff() {
+        f.ajouter("Math",1);
+        f.ajouter("English",2);
+        //acces simple
+        assertEquals(f.accesCoeff("Math"),1.00);
+        assertEquals(f.accesCoeff("English"),2.00);
+        //acces non existant
+        assertEquals(f.accesCoeff("Histoire"),null);
     }
 }
