39a336be932185bab0eb609e1949dadb336bb77c
Packingdustry <alois.mc@hotmail.com>
2024-10-01 16:57:26 +0200

    ajoute le comparateur TriParMerite

--- src/TriParMerite.java
+++ src/TriParMerite.java
@@ -2,7 +2,28 @@ import java.util.Comparator;
 
 public class TriParMerite implements Comparator<Etudiant> {
     @Override
-    public int compare(Etudiant etudiant, Etudiant t1) {
-        return 0;
+    public int compare(Etudiant etu1, Etudiant etu2) {
+
+        double moy1 = 0;
+        try {
+            moy1 = etu1.moyGeneral();
+        } catch (FormationException e) {
+            e.getMessage();
+        }
+
+        double moy2 = 0;
+        try {
+            moy2 = etu2.moyGeneral();
+        } catch (FormationException e) {
+            e.getMessage();
+        }
+
+        int res = 0;
+        if (moy1 < moy2) {
+            res = -1;
+        } else if (moy1 > moy2) {
+            res = 1;
+        }
+        return res;
     }
 }
